d5dab0a50ff66613ee99b8640461b716efe6dc10
Yasmin Marangão <yasmin.marangaog@gmail.com>
2021-10-13 16:04:39 -0400

    CSharp

---
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/3 - CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3___CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto 3 - Criando variaveis ponto flutuate");
+            Console.ReadLine();
+
+            double salario = 1200.70 / 2.0;
+
+            Console.WriteLine("O valor do seu salário é " + salario);
+            Console.ReadLine();
+        }
+    }
+}
