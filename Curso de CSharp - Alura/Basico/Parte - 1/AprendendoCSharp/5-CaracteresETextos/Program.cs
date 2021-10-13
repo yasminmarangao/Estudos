1383ac339a5fb9f2724cfedd8242a39b220d4113
Yasmin Marangão <yasmin.marangaog@gmail.com>
2021-10-13 18:35:26 -0400

    Csharpe

---
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caractere e texto");
+
+            //chacacter
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) 65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia " + 2020;
+            string cursosProgramacao = 
+@"- .NET
+- Java
+- Javascript";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
