6173bd4be240effe0228a66a17c08919862dc3ee
Yasmin Marangão <yasmin.marangaog@gmail.com>
2021-10-20 17:02:44 -0400

    C#

--- Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/11-CalculaPoupanca/Program.cs
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/11-CalculaPoupanca/Program.cs
@@ -15,12 +15,19 @@ namespace _11_CalculaPoupanca
             double valorInvestido = 1000;
             int contadorMes = 1;
 
+
             while (contadorMes <= 12)
             {
                 // 0.36% = 0.0036
                 valorInvestido += valorInvestido * 0.0036;
-                Console.WriteLine($"Após N meses, você terá {valorInvestido}");
 
+                if (contadorMes == 1)
+                    Console.WriteLine($"Após {contadorMes} mês, você terá R${valorInvestido.ToString("n2")} reais");
+                else 
+                    Console.WriteLine($"Após {contadorMes} meses, você terá R${valorInvestido.ToString("n2")} reais");
+                
+                // contadorMes = contadorMes + 1;
+                // contadorMes += 1;
                 contadorMes++;
             } 
 
---
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/P12-CalculadoraPoupanca2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculadoraPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 12");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine($"Após {contadorMes} mês, você tem R${valorInvestido.ToString("n2")} reais");
+                }
+                else
+                {
+                    Console.WriteLine($"Após {contadorMes} meses, você tem R${valorInvestido.ToString("n2")} reais");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
