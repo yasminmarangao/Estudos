7c1ac53ced347b194748b207df22088afc792014
Yasmin Marangão <yasmin.marangaog@gmail.com>
2021-10-20 14:32:41 -0400

    C#

---
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/11-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupanca ");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 0.36% = 0.0036
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine($"Após N meses, você terá {valorInvestido}");
+
+                contadorMes++;
+            } 
+
+            Console.ReadLine();
+        }
+    }
+}
