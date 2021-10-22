f2f7d84df2662f10194a03a0174245149f9ff2c3
Yasmin Marangão <yasmin.marangaog@gmail.com>
2021-10-21 21:45:45 -0400

    C#

---
+++ Curso de CSharp - Alura/Basico/Parte - 1/AprendendoCSharp/MÚLTIPLOSDE3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MÚLTIPLOSDE3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto Multiplos de 3");
+
+
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                if(numero % 3 == 0)
+                {   
+                    Console.WriteLine(numero);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
